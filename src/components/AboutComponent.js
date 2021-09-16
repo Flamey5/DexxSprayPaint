c62a6e6791d735abb30540e05b71bde335b0c18f
Amy Yoder <amyj_510@hotmail.com>
2021-09-16 06:45:59 -0400

    Add to cart modal and about page layout

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -5,12 +5,13 @@ class About extends Component {
   render() {
     return (
       <div className="container mt-5 mb-5" >
-        <div className="row p-5" id="dexxAbout">
-          <div className="col-sm-5">
+        <div className="row p-5 align-items-center" id="dexxAbout">
+        
+          <div className="col-sm-4">
               <img src="/assets/images/profile-pic.jpg" alt="" id="profile-pic" className="d-flex img-thumbnail" />
           </div>
           <div className="col-sm-7">
-              <p className="">Spray painting began as an outlet for Dexx's creative side.
+              <p>Spray painting began as an outlet for Dexx's creative side.
                 The desire to become an artist was not something he expected, but after seeing other
                 artist creating their work he was hooked. He spent hours learning and perfecting the skills necessary to
                 create the amazing paintings he does. He enjoys longboarding, playing fetch with his
--- src/components/GalleryComponent.js
+++ src/components/GalleryComponent.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardBody, CardTitle, Button } from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle, Button, Modal,
+     ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
 import { PAINTINGS } from '../shared/paintings';
+
+
 
 
 class Gallery extends Component {
@@ -9,23 +12,64 @@ class Gallery extends Component {
         super(props);
         this.state = {
             paintings: PAINTINGS,
-            selectedPainting: null
+            selectedPainting: null,
+            ButtonisModalOpen: false
         };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+
+    }
+
+    handleLogin(event) {
+        alert(`Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`);
+        this.toggleModal();
+        event.preventDefault();
     }
 
     render() {
         const gallery = this.state.paintings.map(painting => {
             return (
-                        <div className="col">
-                            <Card className="card">
-                                <CardImg className="galleryImage" width="400px" src={painting.image} alt={painting.name} />
-                                <CardTitle className="cardTitle"><h2>{painting.name}</h2></CardTitle>
-                                <CardBody className="cardBody">
-                                    <Button>Add to Cart</Button>
-                                    <h4 >{painting.price}</h4>
-                                </CardBody>
-                            </Card>
-                        </div>
+                <div className="col mb-5">
+                    <Card className="card">
+                        <CardImg className="galleryImage" width="400px" src={painting.image} alt={painting.name} />
+                        <CardTitle className="cardTitle"><h2>{painting.name}</h2></CardTitle>
+                        <CardBody className="cardBody">
+                        <Button className="btn btn-success" onClick={this.toggleModal}><i className="fa fa-shopping-cart" />{' '}Add to cart</Button>
+                            <h4 >{painting.price}</h4>
+                        </CardBody>
+                    </Card>
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                        <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                        <ModalBody>
+                            <Form onSubmit={this.handleLogin}>
+                                <FormGroup>
+                                    <Label htmlFor="username" >Username</Label>
+                                    <Input type="text" id="username" name="username"
+                                        innerRef={input => this.username = input} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label htmlFor="password" >Password</Label>
+                                    <Input type="password" id="password" name="password"
+                                        innerRef={input => this.password = input} />
+                                </FormGroup>
+                                <FormGroup check>
+                                    <Label check>
+                                        <Input type="checkbox" name="remember"
+                                            innerRef={input => this.remember = input} />
+                                        Remember me
+                                    </Label>
+                                </FormGroup>
+                                <Button type="submit" value="submit" color="primary">Login</Button>
+                            </Form>
+                        </ModalBody>
+                    </Modal>
+                </div>
             );
         });
 
